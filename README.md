a20ecabcca5a03e65d585e544d689f6a13ef75be
Fan <fanj.zh@foxmail.com>
2026-01-29 01:19:52 +0800

    Init.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fancy-OAuth2-Authorization-Server
---
+++ src/main/java/fan/fancy/oauth2/authorization/server/FancyOauth2AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package fan.fancy.oauth2.authorization.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancyOauth2AuthorizationServerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FancyOauth2AuthorizationServerApplication.class, args);
+	}
+
+}
