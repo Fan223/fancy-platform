d9eb357ce11733eab8cc951b48e33319e69746ac
fan <fanj.zh@foxmail.com>
2026-06-13 17:08:37 +0800

    添加 WebClient 配置.

--- fancy-framework/fancy-web-starter/src/main/java/fan/fancy/starter/web/autoconfigure/FancyWebAutoConfiguration.java
+++ fancy-framework/fancy-web-starter/src/main/java/fan/fancy/starter/web/autoconfigure/FancyWebAutoConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
 
 
 /**
@@ -28,5 +30,19 @@ public class FancyWebAutoConfiguration {
         return RestClient.builder()
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
+    }
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder()
+                // 配置策略, 取消内存缓冲区大小限制, 但注意超大数据可能造成内存溢出, 超大数据建议使用流式处理
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(configurer -> configurer
+                                .defaultCodecs().maxInMemorySize(-1)).build());
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return webClientBuilder().build();
     }
 }
